58d96a8c688480351aa5e6fe582f284f73aa6707
Shahbaz Ali <shahbazalivk@gmail.com>
2022-09-27 14:13:55 +0500

    react redux app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux'
+import { incNumber, decNumber } from './actions/index'
 
 function App() {
+  const dispatch = useDispatch()
+  const myState = useSelector((state) => state.changeTheNumber)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center" }}>
+      <h1>React Tutorial for Redux</h1>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "70vh" }}>
+        <button onClick={() => dispatch(decNumber())} className='minus'>-</button>
+        <input className='inputclass' type="text" name="" id="" value={myState} />
+        <button onClick={() => dispatch(incNumber())} className='plus'>+</button>
+      </div>
     </div>
   );
 }
